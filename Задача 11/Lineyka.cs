f5a24a5e5a85606198769e6787c0d1deac8b3a8b
homeworkdome <homeworkdome@outlook.com>
2023-01-13 15:47:02 +0300

    Добавьте файлы проекта.

---
+++ Задача 11/Lineyka.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_11
+{
+    struct Lineyka
+    {
+        double k;
+        double b;
+
+        public Lineyka(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0 || b == 0)
+            {
+                return "Нет решения";
+            }
+            double x = -b / k;
+            return $"Ответ ({x}:0)";
+           
+        }
+    }
+}
---
+++ Задача 11/Program.cs
@@ -0,0 +1,6 @@
+﻿using Задача_11;
+Console.WriteLine("Введите значения коэффициентов для решения линейного уравнения 0=kх+b");
+double k = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+Lineyka lineyka= new Lineyka(k,b);
+Console.WriteLine(lineyka.Root());
